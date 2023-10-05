f1109f18eb079b3ccb254be5c8a53bc7015b8cb6
Fasiha-Mariyam <125477695+Fasiha-Mariyam@users.noreply.github.com>
2023-10-05 10:42:49 +0500

    Add files via upload

---
+++ Mern Stack Duet/day8/app.js
@@ -0,0 +1,24 @@
+// alert("a.salam")
+function change(){
+    document.getElementById("change_me_bro").innerHTML = "<span>day nami misbii fasii together</span>";
+    document.getElementById('hide').style.display="none";
+}
+ function showform()
+{
+    if(select.value == "Student")
+  {
+    document.getElementById("student").style.display="block";
+    document.getElementById("teacher").style.display="none";
+  }
+    
+    else if(select.value == "Teacher")
+    {
+        document.getElementById("student").style.display="none";
+        document.getElementById("teacher").style.display="block";
+      }
+}
+
+// function humsath(){
+//     document.getElementById("together").innerHTML="always cancel kabhi nhi chALTAI"
+// }
+
